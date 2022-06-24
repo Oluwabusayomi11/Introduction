979dfe795f25f8e22fb3bd2ccfe5829c1bb2dd57
Iyekolo Oluwabusayomi <iyekolobusayomi@gmail.com>
2022-06-24 22:48:09 +0100

    Details

---
+++ script.js
@@ -0,0 +1,8 @@
+
+let name = 'Iyekolo Oluwabusayomi Gift';
+let height = "5'4";
+let country = 'Nigeria';
+
+alert("Name: " + name + "    " +
+ "Height: " + height + "   "
+ + "Country: " + country );
